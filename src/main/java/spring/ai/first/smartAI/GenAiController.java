d92be1decfa42c8dcacf1e0dc88a662d45b3156b
kasun Aththanagoda <kasunaththanagoda@gmail.com>
2025-01-27 18:09:38 +0530

    chat service

---
+++ src/main/java/spring/ai/first/smartAI/GenAiController.java
@@ -0,0 +1,26 @@
+package spring.ai.first.smartAI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenAiController {
+
+    ChatService chatService;
+
+    public GenAiController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @GetMapping("ask-ai")
+    public String getResponse(@RequestParam String prompt){
+         return chatService.getResponse(prompt);
+    }
+
+    @GetMapping("ask-ai-options")
+    public String getResponseOptions(@RequestParam String prompt){
+        return chatService.getResponseOptions(prompt);
+    }
+
+}
