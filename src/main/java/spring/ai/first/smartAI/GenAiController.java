eb83bb366e019c2786b577890f5d93e7ed20ecd8
kasun Aththanagoda <kasunaththanagoda@gmail.com>
2025-01-27 18:13:28 +0530

    image service

--- src/main/java/spring/ai/first/smartAI/GenAiController.java
+++ src/main/java/spring/ai/first/smartAI/GenAiController.java
@@ -1,13 +1,20 @@
 package spring.ai.first.smartAI;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class GenAiController {
 
     ChatService chatService;
+
+    ImageService imageService;
 
     public GenAiController(ChatService chatService) {
         this.chatService = chatService;
@@ -21,6 +28,30 @@ public class GenAiController {
     @GetMapping("ask-ai-options")
     public String getResponseOptions(@RequestParam String prompt){
         return chatService.getResponseOptions(prompt);
+    }
+
+      /*@GetMapping("generate-image")
+    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
+    }*/
+
+    @GetMapping("generate-image")
+    public List<String> generateImages(HttpServletResponse response,
+                                       @RequestParam String prompt,
+                                       @RequestParam(defaultValue = "hd") String quality,
+                                       @RequestParam(defaultValue = "1") int n,
+                                       @RequestParam(defaultValue = "1024") int width,
+                                       @RequestParam(defaultValue = "1024") int height) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt, quality, n, width, height);
+
+        // Streams to get urls from ImageResponse
+        List<String> imageUrls = imageResponse.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+                .toList();
+
+        return imageUrls;
     }
 
 }
