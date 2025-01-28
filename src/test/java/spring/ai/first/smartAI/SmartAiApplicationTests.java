e30a729aabc15af4a7bd57b2cc9b26de7fd62a16
kasun Aththanagoda <kasunaththanagoda@gmail.com>
2025-01-29 00:32:28 +0530

    files 2

---
+++ src/test/java/spring/ai/first/smartAI/SmartAiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.ai.first.smartAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
