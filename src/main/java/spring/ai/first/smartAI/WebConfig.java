bff1fb5017fd9f79abb9d271a6d3acecae6e667b
kasun Aththanagoda <kasunaththanagoda@gmail.com>
2025-01-28 23:13:23 +0530

    cors policy resolve

---
+++ src/main/java/spring/ai/first/smartAI/WebConfig.java
@@ -0,0 +1,21 @@
+package spring.ai.first.smartAI;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000/")
+                        .allowCredentials(true)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
