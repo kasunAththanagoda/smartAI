7554cc188469f24b2f981fee2f06f2c653bf7ea8
kasun Aththanagoda <kasunaththanagoda@gmail.com>
2025-01-29 23:10:38 +0530

    main page

--- frontend/smart-ai-frontend/src/App.js
+++ frontend/smart-ai-frontend/src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [activeTab, setActiveTab] = useState('image-generator');
+
+
   return (
     <div className="App">
-    
+      <button className={activeTab === 'image-generator' ? 'active' : ''} 
+      onClick={() => setActiveTab('image-generator')}
+      >Image Generator</button>
+      <button className={activeTab === 'ask-ai' ? 'active' : ''}
+      onClick={() => setActiveTab('ask-ai')}>Ask Ai</button>
+      <button className={activeTab === 'recipe-creator' ? 'active' : ''}
+      onClick={() => setActiveTab('recipe-creator')}> Recipe Generator</button>
+
+      <div>
+        {activeTab === 'image-generator' && (
+          <div>
+            <h2>Image Generator</h2>
+            <input type="text" placeholder="Enter prompt" />
+            <button>Generate Image</button>
+          </div>
+        )}
+        {activeTab === 'ask-ai' && (
+          <div>
+            <h2>Ask Ai</h2>
+            <input type="text" placeholder="Enter prompt" />
+            <button>Ask Ai</button>
+          </div>
+        )}
+        {activeTab === 'recipe-creator' && (
+          <div>
+            <h2>Recipe Generator</h2>
+            <input type="text" placeholder="Enter ingredients" />
+            <button>Generate Recipe</button>
+      </div>
+        )}
+</div>
     </div>
   );
 }
